614324547f35b0802bb8a101a560e8acabd6b7ad
Graziaczek <66276396+Graziaczek@users.noreply.github.com>
2023-07-21 22:15:35 +0200

    Dodaj pliki projektów.

---
+++ Projekt-Programowanie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projekt_Programowanie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
