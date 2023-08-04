c53a39dfbb0ac609fa7214b7c60af8b332c1eaff
Graziaczek <66276396+Graziaczek@users.noreply.github.com>
2023-08-04 15:00:27 +0200

    Vidoki do dema

--- Projekt-Programowanie/Program.cs
+++ Projekt-Programowanie/Program.cs
@@ -7,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
+//builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
+//builder.Services.AddScoped<IWordRepository, WordRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
