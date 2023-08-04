4ac1a97f4ab6cca5731e24b12614226e11c12132
KMilosz <mkrajczok2@gmail.com>
2023-08-04 17:19:27 +0200

    poczatki

--- Projekt-Programowanie/Repository/CrossRepository.cs
+++ Projekt-Programowanie/Repository/CrossRepository.cs
@@ -22,5 +22,24 @@ namespace Projekt_Programowanie.Repository
             return _context.Wzory.OrderBy(p => p.ID_Wzoru).ToList();
         }
 
+        public Wzor getWzor(int id)
+        {
+            return _context.Wzory.Where(p => p.ID_Wzoru == id).FirstOrDefault();
+        }
+
+        public int generowanie(int wzor)
+        {
+            Wzor jaki = getWzor(wzor);
+            int slowo1 = jaki.Slowo1;
+            int slowo2 = jaki.Slowo2;
+            int slowo3 = jaki.Slowo3;
+            int slowo4 = jaki.Slowo5;
+            int slowo5 = jaki.Slowo5;
+            int slowo6 = jaki.Slowo6;
+
+            int[,] tab = new int[8, 8];
+            
+            return 0;
+        }
     }
 }
